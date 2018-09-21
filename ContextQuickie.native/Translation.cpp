5ffb695a7c7dceb2ec8232cf5ff5748b4cfa2ee9
ContextQuickie <ContextQuickie@users.noreply.github.com>
2018-09-21 09:50:15 +0200

    Finished translation for Tortoise Git (Issue #23)

--- ContextQuickie.native/Translation.cpp
+++ ContextQuickie.native/Translation.cpp
@@ -1,9 +1,30 @@
+// ContextQuickie - An eclipse add-on which extens the context menu for accessing various tools.
+
+// Copyright (C) 2009-2014, 2016 - TortoiseGit
+// Copyright (C) 2003-2006, 2008 - TortoiseSVN
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
 #include "stdafx.h"
 #include "contextquickie_tortoise_git_Translation.h"
 
 #include "Windows.h"
 #include <atlstr.h>
 
+// GPL Software usage: The following section was copied from Tortoise Git 2.7.0.0 UniCodeUtils.cpp
+// and adapted for the usage in ContextQuickie.
 #pragma warning(push)
 #pragma warning(disable: 4200)
 struct STRINGRESOURCEIMAGE
@@ -25,8 +46,10 @@ int LoadStringEx(HINSTANCE hInstance, UINT uID, LPTSTR lpBuffer, int nBufferMax,
 #endif
   int ret;
 
-  if (!lpBuffer)
-    return 0;
+  // ContextQuickie: Disabled buffer null pointer check because buffer is alway provided  
+  // if (!lpBuffer)
+  //   return 0;
+
   lpBuffer[0] = L'\0';
   HRSRC hResource = FindResourceEx(hInstance, RT_STRING, MAKEINTRESOURCE(((uID >> 4) + 1)), wLanguage);
   if (!hResource)
@@ -68,6 +91,7 @@ int LoadStringEx(HINSTANCE hInstance, UINT uID, LPTSTR lpBuffer, int nBufferMax,
 #endif
   return ret;
 }
+// End GPL Software usage
 
 JNIEXPORT jstring JNICALL Java_contextquickie_tortoise_git_Translation_getTranslatedString(JNIEnv* env, jobject, jstring library, jlong languageId, jlong menuId, jstring defaultValue)
 {
