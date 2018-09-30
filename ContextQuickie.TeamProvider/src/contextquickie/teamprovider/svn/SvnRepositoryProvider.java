918558ea571ba11398c4db82176bc4d282802deb
ContextQuickie <ContextQuickie@users.noreply.github.com>
2018-09-30 08:33:05 +0200

    Removed ResourceChangeListener code to SvnRepositoryProvider

--- ContextQuickie.TeamProvider/src/contextquickie/teamprovider/svn/SvnRepositoryProvider.java
+++ ContextQuickie.TeamProvider/src/contextquickie/teamprovider/svn/SvnRepositoryProvider.java
@@ -1,24 +1,23 @@
 package contextquickie.teamprovider.svn;
 
 import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
 import org.eclipse.core.resources.IResourceRuleFactory;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.resources.team.ResourceRuleFactory;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.team.core.RepositoryProvider;
 
-public class SvnRepositoryProvider extends RepositoryProvider
+public class SvnRepositoryProvider extends RepositoryProvider implements IResourceChangeListener
 {
   private ResourceRuleFactory resourceRuleFactory = new SvnResourceRuleFactory();
-
-  private ResourceChangeListener resourceChangeListener = new ResourceChangeListener();
 
   /**
    * Default constructor.
    */
   public SvnRepositoryProvider()
   {
-    ResourcesPlugin.getWorkspace().addResourceChangeListener(this.resourceChangeListener, IResourceChangeEvent.POST_CHANGE);
+    ResourcesPlugin.getWorkspace().addResourceChangeListener(this, IResourceChangeEvent.POST_CHANGE);
   }
 
   @Override
@@ -29,7 +28,7 @@ public class SvnRepositoryProvider extends RepositoryProvider
   @Override
   public void deconfigure() throws CoreException
   {
-    ResourcesPlugin.getWorkspace().removeResourceChangeListener(this.resourceChangeListener);
+    ResourcesPlugin.getWorkspace().removeResourceChangeListener(this);
   }
 
   @Override
@@ -42,5 +41,19 @@ public class SvnRepositoryProvider extends RepositoryProvider
   public IResourceRuleFactory getRuleFactory()
   {
     return this.resourceRuleFactory;
+  }
+  
+  @Override
+  public void resourceChanged(IResourceChangeEvent event)
+  {
+    try
+    {
+      event.getDelta().accept(new ResourceDeltaVisitor());
+    }
+    catch (CoreException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 }
