86e41cc3aea0bcd4b797c8b6f7aa410518b7edac
ContextQuickie <ContextQuickie@users.noreply.github.com>
2019-01-19 09:52:39 +0100

    Update to version 0.4.0: Added support for Tortoise SVN 1.9.8 and 1.10.3

--- ContextQuickie.TeamProvider/src/contextquickie/teamprovider/svn/SvnRepositoryProviderType.java
+++ ContextQuickie.TeamProvider/src/contextquickie/teamprovider/svn/SvnRepositoryProviderType.java
@@ -1,7 +1,15 @@
 package contextquickie.teamprovider.svn;
 
+import org.eclipse.team.core.ProjectSetCapability;
 import org.eclipse.team.core.RepositoryProviderType;
 
 public class SvnRepositoryProviderType extends RepositoryProviderType {
+
+  @Override
+  public ProjectSetCapability getProjectSetCapability()
+  {
+    // TODO Auto-generated method stub
+    return super.getProjectSetCapability();
+  }
 
 }
