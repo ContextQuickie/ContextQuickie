6f3d064d12bbf720190858835926aaf118be3188
ContextQuickie <ContextQuickie@users.noreply.github.com>
2020-08-09 18:17:29 +0200

    Updated TortoiseHgMenuBuilder (issue #31)

--- ContextQuickie/src/contextquickie/tortoise/hg/TortoiseHgMenuBuilder.java
+++ ContextQuickie/src/contextquickie/tortoise/hg/TortoiseHgMenuBuilder.java
@@ -1,21 +1,31 @@
 package contextquickie.tortoise.hg;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.function.BiPredicate;
 
-import org.eclipse.jface.action.IContributionItem;
-
-import contextquickie.base.AbstractMenuBuilder;
 import contextquickie.preferences.PreferenceConstants;
+import contextquickie.tortoise.AbstractTortoiseMenuBuilder;
+import contextquickie.tortoise.TortoiseEnvironment;
 import contextquickie.tortoise.TortoiseMenuEntry;
+import contextquickie.tortoise.TortoiseMenuSettings;
 
-public class TortoiseHgMenuBuilder extends AbstractMenuBuilder
+public class TortoiseHgMenuBuilder extends AbstractTortoiseMenuBuilder implements BiPredicate<TortoiseMenuEntry, TortoiseEnvironment>
 {
   /**
    * Tortoise Hg menu configuration.
    */
   private static final List<TortoiseMenuEntry> entries = new ArrayList<TortoiseMenuEntry>();
+  
+  /**
+   * The path to the icons based on the used Tortoise Hg version.
+   */
+  private static final String iconPath = "TortoiseHg/5.5/";
+  
+  /**
+   * The settings for the menu.
+   */
+  private static TortoiseMenuSettings settings = new TortoiseMenuSettings();
 
   static
   {
@@ -24,162 +34,169 @@ public class TortoiseHgMenuBuilder extends AbstractMenuBuilder
             .setCommand("commit")
             .setLabel("Commit...")
             .setHelpText("Commit changes in repository")
-            .setIconPath("menucommit.ico"));
+            .setIconPath(iconPath + "menucommit.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("init")
             .setLabel("Create Repository Here")
             .setHelpText("Create a new repository")
-            .setIconPath("menucreaterepos.ico"));
+            .setIconPath(iconPath + "menucreaterepos.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("clone")
             .setLabel("Clone...")
             .setHelpText("Create clone here from source")
-            .setIconPath("menuclone.ico"));
+            .setIconPath(iconPath + "menuclone.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("status")
             .setLabel("File Status")
             .setHelpText("Repository status & changes")
-            .setIconPath("menushowchanged.ico"));
+            .setIconPath(iconPath + "menushowchanged.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("add")
             .setLabel("Add Files...")
             .setHelpText("Add files to version control")
-            .setIconPath("menuadd.ico"));
+            .setIconPath(iconPath + "menuadd.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("revert")
             .setLabel("Revert Files...")
             .setHelpText("Revert file changes")
-            .setIconPath("menurevert.ico"));
+            .setIconPath(iconPath + "menurevert.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("forget")
             .setLabel("Forget Files...")
             .setHelpText("Remove files from version control")
-            .setIconPath("menurevert.ico"));
+            .setIconPath(iconPath + "menurevert.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("remove")
             .setLabel("Remove Files...")
             .setHelpText("Remove files from version control")
-            .setIconPath("menudelete.ico"));
+            .setIconPath(iconPath + "menudelete.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("rename")
             .setLabel("Rename File")
             .setHelpText("Rename file or directory")
-            .setIconPath("general.ico"));
+            .setIconPath(iconPath + "general.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("workbench")
             .setLabel("Workbench")
             .setHelpText("View change history in repository")
-            .setIconPath("menulog.ico"));
+            .setIconPath(iconPath + "menulog.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("log")
             .setLabel("File History")
             .setHelpText("View change history of selected files")
-            .setIconPath("menulog.ico"));
+            .setIconPath(iconPath + "menulog.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("shelve")
             .setLabel("Shelve Changes")
             .setHelpText("Move changes between working dir and patch")
-            .setIconPath("menucommit.ico"));
+            .setIconPath(iconPath + "menucommit.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("synch")
             .setLabel("Synchronize")
             .setHelpText("Synchronize with remote repository")
-            .setIconPath("menusynch.ico"));
+            .setIconPath(iconPath + "menusynch.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("serve")
             .setLabel("Web Server")
             .setHelpText("Start web server for this repository")
-            .setIconPath("proxy.ico"));
+            .setIconPath(iconPath + "proxy.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("update")
             .setLabel("Update...")
             .setHelpText("Update working directory")
-            .setIconPath("menucheckout.ico"));
+            .setIconPath(iconPath + "menucheckout.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("thgstatus")
             .setLabel("Update Icons")
             .setHelpText("Update icons for this repository")
-            .setIconPath("refresh_overlays.ico"));
+            .setIconPath(iconPath + "refresh_overlays.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("userconf")
             .setLabel("Global Settings")
             .setHelpText("Configure user wide settings")
-            .setIconPath("settings_user.ico"));
+            .setIconPath(iconPath + "settings_user.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("repoconf")
             .setLabel("Repository Settings")
             .setHelpText("Configure repository settings")
-            .setIconPath("settings_repo.ico"));
+            .setIconPath(iconPath + "settings_repo.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("shellconf")
             .setLabel("Explorer Extension Settings")
             .setHelpText("Configure Explorer extension")
-            .setIconPath("settings_user.ico"));
+            .setIconPath(iconPath + "settings_user.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("about")
             .setLabel("About TortoiseHg")
             .setHelpText("Show About Dialog")
-            .setIconPath("menuabout.ico"));
+            .setIconPath(iconPath + "menuabout.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("vdiff")
             .setLabel("Diff to parent")
             .setHelpText("View changes using GUI diff tool")
-            .setIconPath("TortoiseMerge.ico"));
+            .setIconPath(iconPath + "TortoiseMerge.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("hgignore")
             .setLabel("Edit Ignore Filter")
             .setHelpText("Edit repository ignore filter")
-            .setIconPath("ignore.ico"));
+            .setIconPath(iconPath + "ignore.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("guess")
             .setLabel("Guess Renames")
             .setHelpText("Detect renames and copies")
-            .setIconPath("detect_rename.ico"));
+            .setIconPath(iconPath + "detect_rename.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("grep")
             .setLabel("Search History")
             .setHelpText("Search file revisions for patterns")
-            .setIconPath("menurepobrowse.ico"));
+            .setIconPath(iconPath + "menurepobrowse.ico"));
     entries
         .add(new TortoiseMenuEntry()
             .setCommand("dndsynch")
             .setLabel("DnD Synchronize")
             .setHelpText("Synchronize with dragged repository")
-            .setIconPath("menusynch.ico"));
+            .setIconPath(iconPath + "menusynch.ico"));
   }
 
   public TortoiseHgMenuBuilder()
   {
-    super(PreferenceConstants.TORTOISE_HG.getEnabled());
+    super(PreferenceConstants.TORTOISE_HG, settings);
+    settings.setEntries(entries);
+    settings.setMainMenuPrefix("Hg");
+    settings.setSubMenuText("TortoiseHg");
+    settings.setSubMenuIconPath(iconPath + "hg.ico");
+    entries.forEach(entry -> entry.addVisibilityChecker(this));
+    entries.forEach(entry -> entry.setMenuId(1));
+    entries.forEach(entry -> entry.setCommandId("ContextQuickie.commands.TortoiseHg.TortoiseHgCommand"));
   }
 
   @Override
-  protected List<IContributionItem> getMenuEntries()
+  public boolean test(TortoiseMenuEntry t, TortoiseEnvironment u)
   {
     // TODO Auto-generated method stub
-    return Collections.<IContributionItem>emptyList();
+    return true;
   }
 }
